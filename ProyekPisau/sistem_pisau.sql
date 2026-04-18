328472e9e1f7d4c796a57599ebefd41b9ec1d799
Liamarchive <hilya.rosidah06@gmail.com>
2026-04-18 23:18:15 +0700

    Added dummy

--- ProyekPisau/sistem_pisau.sql
+++ ProyekPisau/sistem_pisau.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 13 Apr 2026 pada 04.36
--- Versi server: 10.4.22-MariaDB
--- Versi PHP: 7.3.33
+-- Generation Time: Apr 18, 2026 at 06:16 PM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 7.3.33
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,7 +24,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `jenis_emoney`
+-- Table structure for table `jenis_emoney`
 --
 
 CREATE TABLE `jenis_emoney` (
@@ -33,7 +33,7 @@ CREATE TABLE `jenis_emoney` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `jenis_emoney`
+-- Dumping data for table `jenis_emoney`
 --
 
 INSERT INTO `jenis_emoney` (`id_jenis_emoney`, `nama_layanan`) VALUES
@@ -45,7 +45,7 @@ INSERT INTO `jenis_emoney` (`id_jenis_emoney`, `nama_layanan`) VALUES
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `log_transaksi`
+-- Table structure for table `log_transaksi`
 --
 
 CREATE TABLE `log_transaksi` (
@@ -57,15 +57,19 @@ CREATE TABLE `log_transaksi` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `log_transaksi`
+-- Dumping data for table `log_transaksi`
 --
 
 INSERT INTO `log_transaksi` (`id_transaksi`, `id_user_emoney`, `tipe_transaksi`, `jumlah`, `waktu_transaksi`) VALUES
 (1, 1, 'MASUK', 50000, '2026-04-12 17:10:59'),
-(2, 2, 'KELUAR', 10000, '2026-04-13 02:35:25');
+(2, 2, 'KELUAR', 10000, '2026-04-13 02:35:25'),
+(4, 1, 'MASUK', 100000, '2026-04-13 06:04:21'),
+(5, 3, 'KELUAR', 50000, '2026-04-13 06:08:23'),
+(6, 1, 'MASUK', 50000, '2026-04-17 08:52:48'),
+(7, 1, 'KELUAR', 50000, '2026-04-17 08:56:53');
 
 --
--- Trigger `log_transaksi`
+-- Triggers `log_transaksi`
 --
 DELIMITER $$
 CREATE TRIGGER `update_saldo_after_log` AFTER INSERT ON `log_transaksi` FOR EACH ROW BEGIN
@@ -85,7 +89,7 @@ DELIMITER ;
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `users`
+-- Table structure for table `users`
 --
 
 CREATE TABLE `users` (
@@ -97,17 +101,18 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `users`
+-- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`id_user`, `username`, `password`, `nama_lengkap`, `email`) VALUES
 (1, 'mendezz', 'pass123', 'Shawn Mendes', 'shawn@gmail.com'),
-(2, 'liamarchive', 'secret123', 'Lia M', 'liam@gmail.com');
+(2, 'liamarchive', 'secret123', 'Lia M', 'liam@gmail.com'),
+(3, 'fatson', 'todd23', 'Jason Todd', 'jasont@gmail.com');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `user_emoney`
+-- Table structure for table `user_emoney`
 --
 
 CREATE TABLE `user_emoney` (
@@ -115,30 +120,36 @@ CREATE TABLE `user_emoney` (
   `id_user` int(11) DEFAULT NULL,
   `id_jenis_emoney` int(11) DEFAULT NULL,
   `nomor_identitas` varchar(50) DEFAULT NULL,
+  `pass_emoney` varchar(100) DEFAULT NULL,
   `saldo` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `user_emoney`
+-- Dumping data for table `user_emoney`
 --
 
-INSERT INTO `user_emoney` (`id_user_emoney`, `id_user`, `id_jenis_emoney`, `nomor_identitas`, `saldo`) VALUES
-(1, 2, 1, '1200019800313', 1050000),
-(2, 2, 4, '089604338348', 90000),
-(3, 2, 3, '089604338348', 250000);
+INSERT INTO `user_emoney` (`id_user_emoney`, `id_user`, `id_jenis_emoney`, `nomor_identitas`, `pass_emoney`, `saldo`) VALUES
+(1, 2, 1, '1200019800313', '19800313', 1150000),
+(2, 2, 4, '089604338348', '38348', 90000),
+(3, 2, 3, '089604338348', '89604', 200000),
+(4, NULL, 2, '8897921302141', '02141', 1000000),
+(5, NULL, 3, '083255673835', '73835', 1000000),
+(6, NULL, 1, '1230076112980', '112980', 1000000),
+(7, NULL, 2, '230006711207', '11207', 1000000),
+(8, NULL, 3, '082173419820', '19820', 1000000);
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indeks untuk tabel `jenis_emoney`
+-- Indexes for table `jenis_emoney`
 --
 ALTER TABLE `jenis_emoney`
   ADD PRIMARY KEY (`id_jenis_emoney`);
 
 --
--- Indeks untuk tabel `log_transaksi`
+-- Indexes for table `log_transaksi`
 --
 ALTER TABLE `log_transaksi`
   ADD PRIMARY KEY (`id_transaksi`),
@@ -146,14 +157,14 @@ ALTER TABLE `log_transaksi`
   ADD KEY `id_user_emoney` (`id_user_emoney`);
 
 --
--- Indeks untuk tabel `users`
+-- Indexes for table `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id_user`),
   ADD UNIQUE KEY `username` (`username`);
 
 --
--- Indeks untuk tabel `user_emoney`
+-- Indexes for table `user_emoney`
 --
 ALTER TABLE `user_emoney`
   ADD PRIMARY KEY (`id_user_emoney`),
@@ -161,45 +172,45 @@ ALTER TABLE `user_emoney`
   ADD KEY `id_jenis_emoney` (`id_jenis_emoney`);
 
 --
--- AUTO_INCREMENT untuk tabel yang dibuang
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT untuk tabel `jenis_emoney`
+-- AUTO_INCREMENT for table `jenis_emoney`
 --
 ALTER TABLE `jenis_emoney`
   MODIFY `id_jenis_emoney` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
--- AUTO_INCREMENT untuk tabel `log_transaksi`
+-- AUTO_INCREMENT for table `log_transaksi`
 --
 ALTER TABLE `log_transaksi`
-  MODIFY `id_transaksi` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_transaksi` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
--- AUTO_INCREMENT untuk tabel `users`
+-- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- AUTO_INCREMENT untuk tabel `user_emoney`
+-- AUTO_INCREMENT for table `user_emoney`
 --
 ALTER TABLE `user_emoney`
-  MODIFY `id_user_emoney` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_user_emoney` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
--- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+-- Constraints for dumped tables
 --
 
 --
--- Ketidakleluasaan untuk tabel `log_transaksi`
+-- Constraints for table `log_transaksi`
 --
 ALTER TABLE `log_transaksi`
   ADD CONSTRAINT `log_transaksi_ibfk_1` FOREIGN KEY (`id_user_emoney`) REFERENCES `user_emoney` (`id_user_emoney`);
 
 --
--- Ketidakleluasaan untuk tabel `user_emoney`
+-- Constraints for table `user_emoney`
 --
 ALTER TABLE `user_emoney`
   ADD CONSTRAINT `user_emoney_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_user`),
