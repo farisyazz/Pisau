055bcf296f8179cabef9443f7bc29c275b0f6e61
farisya <farisyasatyau@gmail.com>
2026-04-20 12:02:11 +0700

    Delete MainSistem.java

--- MainSistem.java
+++
@@ -1,9 +0,0 @@
-package proyekpisau;
-import javafx.application.Application;
-
-public class MainSistem {
-    public static void main(String[] args){
-        // This launches the Authentication class start() method
-        Application.launch(Authentication.class, args);
-    }
-}
